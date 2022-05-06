bdcaaabeb34c4660cb3250e094a1ea6d17e125c5
Iain Ballard <iain.ballard@ewater.services>
2022-05-06 16:31:43 +0100

    stub

--- tich/libtich/TichRenderer.cs
+++ tich/libtich/TichRenderer.cs
@@ -5,5 +5,18 @@
 /// </summary>
 public static class TichRenderer
 {
-    
+    /// <summary>
+    /// Render a single layer's sub-program to a rectangular array.
+    /// Values in the array are between 0.0 (entirely out of the object)
+    /// and 1.0 (entirely inside the object);
+    ///
+    /// P value of (0,0) is in the centre of the array.
+    ///
+    /// 
+    /// </summary>
+    public static double[,] RenderLayer(TichProgram prog, int width, int height)
+    {
+        // simple scan-wise stepping algorithm
+        var outp = new double[width,height];
+    }
 }
